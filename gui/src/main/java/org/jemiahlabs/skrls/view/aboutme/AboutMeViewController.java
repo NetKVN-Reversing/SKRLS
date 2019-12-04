368e6fc762bccac6f8787f97f710b24858e7390e
Alan M.E <alanmarquez@outlook.com>
2019-12-04 17:04:30 -0500

    removed unnecessary import

--- gui/src/main/java/org/jemiahlabs/skrls/view/aboutme/AboutMeViewController.java
+++ gui/src/main/java/org/jemiahlabs/skrls/view/aboutme/AboutMeViewController.java
@@ -1,6 +1,5 @@
 package org.jemiahlabs.skrls.view.aboutme;
 
-import org.jemiahlabs.skrls.view.base.EventArgs;
 import org.jemiahlabs.skrls.view.base.SubWindow;
 import org.jemiahlabs.skrls.view.base.javafxwindows.StageController;
 
